bf24d1e7e69a3ae003a272d4e928cca81e80b7f4
Khoshgozaran <jeff@createnu.com>
2013-03-09 19:11:35 -0800

    first setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '5e3133d3808088de070eccb7c15fdd2db05dcfb0b595bd20bb02bbc5f5b1c68fd2e0cce0373b93c2e53a1031ea5b7eca780f0255169b6c85d44549b6173106e0'
